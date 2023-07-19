acf13b9c45d4edd96af6bfd0376c3c5152bc6c15
agmercier <arthur.g.mercier@gmail.com>
2023-07-19 14:21:20 +0200

    display platform logo and critics score

---
+++ src/Components/GameCards.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+
+interface Props {
+  game: Game;
+}
+
+const GameCards = ({ game }: Props) => {
+  return (
+    <Card borderRadius={"10px"} overflow={"hidden"}>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <HStack justify={"space-between"}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          ></PlatformIconList>
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCards;
